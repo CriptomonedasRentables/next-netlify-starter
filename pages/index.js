f842c2c0449669c85347963c83c68313e820eb80
Criptomonedas Rentables ✅com <71561780+CriptomonedasRentables@users.noreply.github.com>
2022-07-16 13:34:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bienvenido a mi app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>CriptomonedasRentables.com</code>
         </p>
       </main>
 
